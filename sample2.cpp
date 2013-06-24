43deb808502412d7a5ef642538ff72fabaa7dd0c
Takeshi Nagae <takeshi.nagae@gmail.com>
2013-06-24 15:00:08 +0900

    sample1.cpp と sample2.cpp を追加

---
+++ sample2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main ( void ) {
+  double latitude, longitude;
+  cout << "緯度と経度を入力して下さい" << endl;
+  cin >> latitude >> longitude;
+  cout << "緯度:" << latitude << " 経度:" << longitude << endl;
+  
+}
