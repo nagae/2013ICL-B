809cb7cc99252767bfc91a93b9c5ddd37c439135
Takeshi Nagae <takeshi.nagae@gmail.com>
2013-06-25 16:44:05 +0900

    sample2.cpp を少し修正

--- sample2.cpp
+++ sample2.cpp
@@ -2,9 +2,10 @@
 using namespace std;		// 名前空間 std をデフォルトで利用する
 
 // メインルーチン
-int main ( void ){ // 引数無しで整数(int)型の戻り値を返す main 関数を定義
+int main ( void ) // 引数無しで整数(int)型の戻り値を返す main 関数を定義
+{ 
   double latitude, longitude; // 2つの倍精度実数型の変数 latitude, longitude を定義
   cout << "緯度と経度を入力して下さい" << endl; // 標準出力 cout に緯度・経度の入力を促すメッセージを表示
   cin >> latitude >> longitude;	// 標準入力から latitude と longitude の値を獲得
-  cout << "緯度:" << latitude << " 経度:" << longitude << endl;
+  cout << "緯度:" << latitude << " 経度:" << longitude << endl; // 文字列と組み合わせて表示
 }
