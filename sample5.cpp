a8d184e202ceab5ec32d56bbb3d6519cfffad006
Takeshi Nagae <takeshi.nagae@gmail.com>
2013-07-04 20:13:14 +0900

    sample5.cpp を shift_jis-unix に

--- sample5.cpp
+++ sample5.cpp
@@ -1,26 +1,26 @@
-#include <iostream>		// 標準入出力用ライブラリを読み込む
-#include <iomanip> 		// 入出力表示精度を指定するためのライブラリを読み込む
-#include <fstream>	       	// ファイル入出力用ライブラリを読み込む
-#include <string>		// 文字列用ライブラリを読み込む
+#include <iostream>		// �W�����o�͗p���C�u������ǂݍ���
+#include <iomanip> 		// ���o�͕\�����x���w�肷�邽�߂̃��C�u������ǂݍ���
+#include <fstream>	       	// �t�@�C�����o�͗p���C�u������ǂݍ���
+#include <string>		// ������p���C�u������ǂݍ���
 
-using namespace std;		// 名前空間 std をデフォルトで利用する
+using namespace std;		// ���O��� std ���f�t�H���g�ŗ��p����
 
-// メインルーチン
-int main ( void ) { // 引数無しで整数(int)型の戻り値を返す main 関数を定義
+// ���C�����[�`��
+int main ( void ) { // ���������Ő���(int)�^�̖߂�l��Ԃ� main �֐����`
   cout << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl;
   cout << "<kml xmlns=\"http://www.opengis.net/kml/2.2\">" << endl;
   cout << "  <Folder>" << endl;
   
-  ifstream fin("bus-short.csv"); // ファイル入力ストリーム
-  while ( !fin.eof() ) {	 // fin がファイルの終わりまできていない間繰り返す
-    // ファイルから1行読み込んで文字列に一時保管
-    string line;		 // 読み込み用文字列
-    getline(fin, line);	 // 1行読み込む
+  ifstream fin("bus-short.csv"); // �t�@�C�����̓X�g���[��
+  while ( !fin.eof() ) {	 // fin ���t�@�C���̏I���܂ł��Ă��Ȃ��ԌJ��Ԃ�
+    // �t�@�C������1�s�ǂݍ���ŕ�����Ɉꎞ�ۊ�
+    string line;		 // �ǂݍ��ݗp������
+    getline(fin, line);	 // 1�s�ǂݍ���
 
-    // 一時保管された文字列からバス停データを読み込む
-    int ID;			// バス停ID用の整数型の変数を定義
-    char name[100];		// バス停名用の文字列型の変数を定義
-    double latitude, longitude; // 緯度・経度用の2つの倍精度実数型の変数を定義
+    // �ꎞ�ۊǂ��ꂽ�����񂩂�o�X��f�[�^��ǂݍ���
+    int ID;			// �o�X��ID�p�̐����^�̕ϐ����`
+    char name[100];		// �o�X�▼�p�̕�����^�̕ϐ����`
+    double latitude, longitude; // �ܓx�E�o�x�p��2�̔{���x�����^�̕ϐ����`
     sscanf(line.c_str(),
 	   "%d,%lf,%lf,%[^\n]",
 	   &ID, &latitude, &longitude, name
